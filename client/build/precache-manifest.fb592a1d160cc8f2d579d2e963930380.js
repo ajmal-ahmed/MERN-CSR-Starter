ed910ef8cf5d5669099d8fb01dc0a78d679eba09
Ajmal Ahamed <ajmalahmed123@gmail.com>
2020-08-04 21:10:48 +0530

    client build folder incl

---
+++ client/build/precache-manifest.fb592a1d160cc8f2d579d2e963930380.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d744878c8c513bdd78b3efd014b4faf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a5ef9059d1499a45fe3",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "fe3aa6de77afa35cf3dc",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3a5ef9059d1499a45fe3",
+    "url": "/static/js/2.57505156.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.57505156.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe3aa6de77afa35cf3dc",
+    "url": "/static/js/main.c4d62914.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
